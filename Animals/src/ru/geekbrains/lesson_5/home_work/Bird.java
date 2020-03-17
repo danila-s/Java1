65ce6048fbe2568a66e118c9bd8b940084699710
Danila Shloma <ds.shloma@gmail.com>
2020-03-17 15:58:13 +0300

    !

---
+++ Animals/src/ru/geekbrains/lesson_5/home_work/Bird.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson_5.home_work;
+
+public class Bird extends Animal {
+
+    public Bird () {
+        super(5, 0, 0.2);
+    }
+    public Bird(int run_distance, int swim_distance, double jump_distance) {
+        super(run_distance, swim_distance, jump_distance);
+
+    }
+    }
+
+
---
+++ Animals/src/ru/geekbrains/lesson_5/home_work/Cat.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson_5.home_work;
+
+public class Cat extends Animal {
+    public Cat() {
+        super(200, 0, 2);
+    }
+
+    public Cat(int run_distance, int swim_distance, double jump_distance) {
+        super(run_distance, swim_distance, jump_distance);
+
+    }
+}
